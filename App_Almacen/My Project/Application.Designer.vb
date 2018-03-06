1b78a3794578388b4e490a5e52ce099a3bfc4077
Carlos Gaytán <al02710429@tecmilenio.mx>
2018-03-06 11:47:23 -0600

    Se inicia con las Apps de Almacén para alta de Materia Prima

--- App_Almacen/My Project/Application.Designer.vb
+++ App_Almacen/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.App_Almacen.Form1
+            Me.MainForm = Global.App_Almacen.Principal
         End Sub
     End Class
 End Namespace
--- App_Almacen/Principal.Designer.vb
+++ App_Almacen/Principal.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Principal
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,19 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Principal))
+        Me.SuspendLayout()
+        '
+        'Principal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(957, 567)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Principal"
+        Me.Text = "Apps Almacén"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
